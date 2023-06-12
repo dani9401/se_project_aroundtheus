1966474c709913b0105d5de952d23b642cc22871
Danielle Foss <danifoss2011@gmail.com>
2023-06-11 21:23:59 -0600

    fix this. error on inactiveButtonClass

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -36,7 +36,7 @@ export default class FormValidator {
     if (!_isFormValid) {
       this.disableButton();
     } else {
-      this._submitButton.classList.remove(_inactiveButtonClass);
+      this._submitButton.classList.remove(this._inactiveButtonClass);
       this._submitButton.disabled = false;
     }
   }
