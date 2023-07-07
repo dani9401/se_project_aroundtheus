3dd6ac84955b321668784905a54e151b9df90709
Danielle Foss <danifoss2011@gmail.com>
2023-07-07 13:41:40 -0600

    refactored constructor

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,8 +1,8 @@
 export default class Section {
-  constructor({ items, renderer }, selector) {
+  constructor({ items, renderer }, container) {
     this._items = items;
     this._renderer = renderer;
-    this._container = selector;
+    this._container = container;
   }
 
   renderItems() {
