fbe4f73bb50a669de3c40647d452568857602dee
Danielle Foss <danifoss2011@gmail.com>
2023-07-14 13:31:21 -0600

    add instance and other base code. update token

--- src/api.js
+++ src/api.js
@@ -1,10 +1,21 @@
 class Api {
   constructor(options) {
-    // constructor body
+    this._baseURL = options.baseUrl;
+    this._authToken = options.headers.authorization;
+    this._contentType = options.headers.contentType;
   }
 
   getInitialCards() {
-    // ...
+    return fetch("https://around.nomoreparties.co/v1/cohort-3-en/cards", {
+      headers: {
+        authorization: "61d2a1b6-82a5-4ea1-a1a2-2a63d3c4120b",
+      },
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Error: ${res.status}`);
+    });
   }
 
   // other methods for working with the API
@@ -17,3 +28,10 @@ const api = new Api({
     "Content-Type": "application/json",
   },
 });
+
+api
+  .getInitialCards()
+  .then((result) => {})
+  .catch((err) => {
+    console.error(err); // log the error to the console
+  });
